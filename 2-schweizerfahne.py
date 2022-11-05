7e8ec42411b6534f776be5f6251ac92406ba15a0
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-11-05 10:22:06 +0100

    task2 swiss flag

--- 2-schweizerfahne.py
+++ 2-schweizerfahne.py
@@ -5,3 +5,9 @@ Erzeuge eine Grafik der Schweizerfahne und zeige diese an.
 from pytamaro.de import rechteck, rot, weiss, drehe, ueberlagere, zeige_grafik
 
 # schreib hier Deine Lösung von Aufgabe 2
+
+SEITE = 320
+grund = rechteck(SEITE, SEITE, rot)
+horizontal_arm = rechteck(SEITE // 32 * 20, SEITE // 32 * 6, weiss)
+vertikal_arm = rechteck(SEITE//32*6,SEITE //32*20,weiss)
+zeige_grafik(ueberlagere(ueberlagere (vertikal_arm,horizontal_arm),grund))
