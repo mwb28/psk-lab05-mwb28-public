6c27bc27d0dc27453e56f5168183f878dafccfe8
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-11-05 15:01:28 +0100

    task5 draw  ghost

--- 5-gespenst.py
+++ 5-gespenst.py
@@ -2,5 +2,19 @@
 Erzeuge eine Grafik eines Pacman-Gespensts
 """
 
-from pytamaro.de import zeige_grafik
+from pytamaro.de import zeige_grafik, dreieck, drehe, kreis_sektor, rechteck, neben, ueber, gruen, leere_grafik
+
+BREITE = 400
+HOEHE = 200
+DREIECK= drehe(180,dreieck(BREITE//6,gruen))
+RECHTECK= rechteck(BREITE,HOEHE,gruen)
+HALBKREIS= drehe(180,kreis_sektor(BREITE//2,180,gruen))
+zeichne_zacken = leere_grafik()
+for i in range(3):
+    zeichne_zacken= neben(zeichne_zacken,neben(DREIECK,DREIECK))
+
+
+
+zeige_grafik(ueber(HALBKREIS,ueber(RECHTECK,zeichne_zacken)))
+
 
