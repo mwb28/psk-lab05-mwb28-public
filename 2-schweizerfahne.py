e334a77cb681c3311789f38c3a539e40a0793a33
mwb28 <97355066+mwb28@users.noreply.github.com>
2022-11-05 10:26:08 +0100

    task2 with function drehe

--- 2-schweizerfahne.py
+++ 2-schweizerfahne.py
@@ -10,4 +10,4 @@ SEITE = 320
 grund = rechteck(SEITE, SEITE, rot)
 horizontal_arm = rechteck(SEITE // 32 * 20, SEITE // 32 * 6, weiss)
 vertikal_arm = rechteck(SEITE//32*6,SEITE //32*20,weiss)
-zeige_grafik(ueberlagere(ueberlagere (vertikal_arm,horizontal_arm),grund))
+zeige_grafik(ueberlagere(ueberlagere (drehe(90,horizontal_arm),horizontal_arm),grund))
