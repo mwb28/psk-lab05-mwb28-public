48bc3c2cb5b0e1d6b3c7efbc3b9dcb1583ca9149
Matthias WaltherBueel <youremail@yourdomain.comgit config --global user.name Your>
2022-11-16 10:00:41 +0100

    task1, add function and comment

--- 1-deutsche-flagge.py
+++ 1-deutsche-flagge.py
@@ -2,15 +2,23 @@
 Erzeuge eine Grafik der deutschen Flagge und zeige diese an.
 """
 
-from pytamaro.de import rechteck, rgb_farbe, ueber, zeige_grafik
+from pytamaro.de import rechteck, rgb_farbe, ueber, zeige_grafik, Grafik
 
 # komplettiere hier Deine Lösung von Aufgabe 1
-BREITE = 500
-HOEHE = BREITE * 3 // 5
-FARBEN = [rgb_farbe(0, 0, 0), rgb_farbe(255, 0, 0), rgb_farbe(255, 204, 00)]
+BREITE : int= 500
+HOEHE : int= BREITE * 3 // 5
+FARBEN : list = [rgb_farbe(0, 0, 0), rgb_farbe(255, 0, 0), rgb_farbe(255, 204, 00)]
 
-schwarzes_rechteck = rechteck(BREITE, HOEHE // 3, FARBEN[0])
-rotes_rechteck = rechteck (BREITE, HOEHE//3, FARBEN[1] )
-goldenes_rechteck = rechteck (BREITE, HOEHE//3, FARBEN[2] )
+def zeichne_deutsche_flagge()-> Grafik:
+    """
+    Erzeugt eine deutsche Flagge mit der Breite 500 px.
+    @returns: die deutsche Flagge als Grafik
+    """
+    schwarzes_rechteck = rechteck(BREITE, HOEHE // 3, FARBEN[0])    
+    rotes_rechteck = rechteck (BREITE, HOEHE//3, FARBEN[1] )    
+    goldenes_rechteck = rechteck (BREITE, HOEHE//3, FARBEN[2] )
 
-zeige_grafik(ueber(schwarzes_rechteck, ueber(rotes_rechteck,goldenes_rechteck))) 
+    return ueber(schwarzes_rechteck, ueber(rotes_rechteck,goldenes_rechteck))
+
+
+zeige_grafik(zeichne_deutsche_flagge()) 
